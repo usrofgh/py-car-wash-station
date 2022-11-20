ce8e16eec7fd67dc27b920eb64a80867e1ac5808
usrofgh <mailforgimail@gmail.com>
2022-11-20 18:34:25 +0200

    the word "do" was changed to write

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ class CarWashStation:
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
-        # I don't know how to do here correctly
+        # I don't know how to write here correctly
         return round(car.comfort_class * ((self.clean_power - car.clean_mark) * (self.average_rating
                                                              / self.distance_from_city_center)), 1)
 
